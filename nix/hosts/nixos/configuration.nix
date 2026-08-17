4174f2bfde522f6947bc365ef7dbcc269df5f19a
serbinjulius <serbinjulius@gmail.com>
2026-08-17 12:50:34 +0000

    nix: add CIFS mount configuration for /data

--- nix/hosts/nixos/configuration.nix
+++ nix/hosts/nixos/configuration.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 {
+
   imports = [ ./hardware-configuration.nix ];
   
   networking.hostName = "nixos";
@@ -14,5 +15,18 @@
     extraGroups = [ "wheel" ];
   };
   users.groups.serbinjulius = {};
+
+  fileSystems."/data" = {
+    device = "//192.168.0.3/data";
+    fsType = "cifs";
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in [
+      "credentials=/etc/nixos/smb-secrets"
+      "uid=1000" "gid=100"
+      automount_opts
+    ];
+  };
+
 }
 
