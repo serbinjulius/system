a1b911bea446e91c5d4021b81fa4c992a2bbc3e9
serbinjulius <serbinjulius@gmail.com>
2026-09-18 00:18:21 +0000

    emacs: add doom theme and disable scrollbars/fringes

--- configs/emacs/init.el
+++ configs/emacs/init.el
@@ -24,3 +24,19 @@
 	     :after evil
 	     :config
 	     (evil-collection-init))
+
+;; theme
+(use-package doom-themes
+  :ensure t
+  :config
+  (setq doom-themes-enable-bold t
+	doom-themes-enable-italic t)
+  (load-theme 'doom-one t)
+  (doom-themes-visual-bell-config)
+  (doom-themes-org-config))
+
+;; visual elements
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(fringe-mode 0)
